dd471591dfdd153c16c9c2d40a5101c89956ce59
ergegananputra <ergegananputra@gmail.com>
2025-04-15 21:39:38 +0700

    feat: add option to save analyzed frames in test_masibelajar_model

--- test/test_models.py
+++ test/test_models.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import os
 from weakref import ref
 import cv2
 import numpy as np
@@ -34,13 +35,19 @@ def test_analyze_pose_video(model : PoseModel, image_path: str):
     model.analyze_video(image_path)
 
 @show_func_name
-def test_masibelajar_model(image_path: str, safezone: list):
+def test_masibelajar_model(image_path: str, safezone: list, save: bool = False):
     model = MasiBelajarModel(
         od_weight='app/models/object_detection/config/best.pt',
         pose_weight='app/models/key_points/config/yolo11m-pose.pt',
         tracker='app/models/tracker/tracker.yaml',
     )
 
+    if save:
+        output_dir = "storages/output"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_path = os.path.join(output_dir, os.path.basename(image_path))
+        _counter = 0
 
     for summary, frame in model.analyze_frame(
         inference_path=image_path, 
@@ -53,6 +60,11 @@ def test_masibelajar_model(image_path: str, safezone: list):
         ):
 
         cv2.imshow("Frame", frame)
+
+        if save:
+            _counter += 1
+            cv2.imwrite(os.path.join(output_dir, f"{_counter}.jpg"), frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -145,18 +157,18 @@ def test_tracking(img: str):
             break
 
 if __name__ == '__main__':
-    safezone = [[277, 142],[299, 149],[454, 140],[449, 219],[301, 244],[277, 225]]
+    # safezone = [[277, 142],[299, 149],[454, 140],[449, 219],[301, 244],[277, 225]]
     # safezone = [[442,30], [496, 33], [488,201], [437, 205]]
     # safezone = [[319, 49], [478, 114], [479, 328], [328, 407]] # Scena 1
     # safezone = [[559, 138], [686, 172], [650, 508], [521, 534]] # Scena 2
-    # safezone = [[696, 210], [1200, 130], [1166, 716], [1009, 718], [705, 567]] # Scena 3
+    safezone = [[696, 210], [1200, 130], [1166, 716], [1009, 718], [705, 567]] # Scena 3
 
     # image_path = 'test/data/Fall.mp4'
-    image_path = 'test/data/TikTokToddler.mp4'
+    # image_path = 'test/data/TikTokToddler.mp4'
     # image_path = 'https://www.youtube.com/live/yNKvkPJl-tg?feature=shared'
     # image_path = '/mnt/d/UGM/EDU/SamsungInnovationCampus/Dataset/Scena1.mp4'
-    image_path = '/mnt/d/UGM/EDU/SamsungInnovationCampus/Dataset/Scena2.mp4'
-    image_path = 'http://192.168.122.82:81/stream'
+    # image_path = '/mnt/d/UGM/EDU/SamsungInnovationCampus/Dataset/Scena2.mp4'
+    # image_path = 'http://192.168.122.82:81/stream'
     image_path = '/mnt/d/UGM/EDU/SamsungInnovationCampus/Dataset/Stream.mp4'
 
 
@@ -169,7 +181,7 @@ if __name__ == '__main__':
     # test_analyze_pose_video(poseModel, image_path)
 
 
-    test_masibelajar_model(image_path, safezone)
+    test_masibelajar_model(image_path, safezone, True)
     # test_masibelajar_model_2(image_path, safezone)
 
     # test_tracking(image_path)
